3e891444a5ca9c072b670689cc844585ebcfed83
Pushpak Panchal <pushpak.net@gmail.com>
2020-12-20 16:10:36 -0600

    End of Section 4 - MediatR + CQRS backend CRUD

---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using domain;
+using MediatR;
+using Persistence;
+
+
+namespace Application.Activities
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            public DataContext _context { get; }
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                if (activity == null)
+                {
+                    throw new Exception("Could not find activity");
+                }
+
+                _context.Activities.Remove(activity);
+                var success = await _context.SaveChangesAsync() > 0;
+                if (success) return Unit.Value;
+
+                throw new Exception("Problem Saving Activity");
+
+            }
+        }
+    }
+}
